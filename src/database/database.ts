2eacd6fa263736a562d4357caeae351656daf39c
igustavo11 <gustavobrun1224@gmail.com>
2025-08-25 08:54:37 -0300

    feat: db conection models

---
+++ src/database/database.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://admin:admin123@localhost:27027/meubanco?authSource=admin")
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+
+db.once(
+    'open',
+    () => {
+        console.log('conected mongodbbb');
+    }
+)
+
+export default db;
---
+++ src/models/doctor.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const doctorSchema = new Schema ({
+    doctorId: { type: String, required: [true, 'Doctor ID is required'] },
+    name: { type: String, required: [true, 'Name is required'] },
+    login: { type: String, required: [true, 'Login is required'], unique: true },
+    password: { type: String, required: [true, 'Password is required'] },
+    specialty: { type: String, required: [true, 'Specialty is required']},
+    crm: { type: String, required: [true, 'CRM is required'], unique: true },
+    email: { type: String, required: [true, 'Email is required']},
+    phone: { type: String, required: [true, 'Phone is required']},
+    createdAt: { type: Date, default: Date.now },
+});
+
+
+const doctor = mongoose.model('doctor', doctorSchema);
+
+export default doctor;
